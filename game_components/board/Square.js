e805a73b58374d0f5d26db650a15c63ebc91eab4
Nathan Cairns <nathancairns777@gmail.com>
2018-12-18 23:19:52 +1300

    Add square component class

---
+++ game_components/board/Square.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+
+export default class Square extends React.Component {
+    static PropTypes = {
+        
+    }
+
+    render () {
+        <View>
+            <Text>[]</Text>
+        </View>
+    }
+
+}
+
+const styles = StyleSheet.create({
+
+});
